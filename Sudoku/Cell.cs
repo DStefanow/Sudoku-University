e7ba13909dd2081ee1a218e1c009acf0763f4f0a
mopak <dobromirstefanov96@gmail.com>
2018-05-26 11:56:09 +0300

    Added properties

--- Sudoku/Cell.cs
+++ Sudoku/Cell.cs
@@ -1,12 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace Sudoku
 {
-    class Cell
+    class Cell : INotifyPropertyChanged
     {
+        // Implemented from interface
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool readOnlyValue = false;
+        private bool isValidValue = true;
+        private int? valueValue = null;
+
+        public bool ReadOnly
+        {
+            get { return readOnlyValue; }
+            set
+            {
+                if (readOnlyValue != value)
+                {
+                    readOnlyValue = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("ReadOnly"));
+                    }
+                }
+            }
+        }
+
+        public int? Value
+        {
+            get { return valueValue; }
+            set
+            {
+                if (valueValue != value)
+                {
+                    valueValue = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Value"));
+                    }
+                }
+            }
+        }
+
+        public bool IsValid
+        {
+            get { return isValidValue; }
+            set
+            {
+                if (isValidValue != value)
+                {
+                    isValidValue = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("IsValid"));
+                    }
+                }
+            }
+        }
     }
 }
